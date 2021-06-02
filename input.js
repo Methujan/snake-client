9be29c8c700c20080500bac33bc49e16bb6628b3
Methujan Jeyapalan <methujan_02@hotmail.com>
2021-06-02 22:34:15 +0000

    snake games with movements

--- input.js
+++ input.js
@@ -1,3 +1,5 @@
+const { stdout } = require("process");
+
 let connection;
 
 const setupInput = function (conn) {
@@ -15,7 +17,11 @@ const handleUserInput = function(key) {
   // your code here
   if((key === 'w')){
     connection.write("Move: up");
+    setInterval(() => {
+      
+    }, 100);
   }
+  
   if((key === 'a')){
     connection.write("Move: left");
   }
@@ -25,8 +31,20 @@ const handleUserInput = function(key) {
   if((key === 'd')){
     connection.write("Move: right");
   }
+  if((key === 'q')){
+    connection.write("Say: No no noo noooo");
+  }
+  if((key === 'o')){
+    connection.write("Say: :O");
+  }
+  if((key === 'u')){
+    connection.write("Say: :)");
+  }
+
+
 
   if (key === '\u0003') { // ctrl + c
+    stdout.write('Exited the game. Tata. \n')
     process.exit(); 
   } 
 };
--- play.js
+++ play.js
@@ -2,7 +2,7 @@ const {connect} = require('./client');
 const {setupInput} = require('./input');
 
 console.log("Connecting ...");
-connect();
+const connection = connect();
 
 
-setupInput(connect); //Calling setupInput
+setupInput(connection); //Calling setupInput
